73a0b5f0255e95ab67b8641784f30bceffac4b81
Jorgen Hookham <Jorgen@Hookham.co>
2016-06-08 11:10:32 +0300

    settings; splash photo

--- app/W8M8/settings.py
+++ app/W8M8/settings.py
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'W8M8.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}  # No database. All google sheets.
 
 
 # Password validation
